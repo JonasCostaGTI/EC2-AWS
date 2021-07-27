24696c1e5b7bf42789b74508c1f7b63cd20e0e3e
Jonas <jonas.costa@icloud.com>
2021-07-27 16:54:09 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = "ami-0b9064170e32bde34"
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [ "security_jenkins_port"]
+  tags= {
+    Name = "jenkins_instance"
+  }
+}
